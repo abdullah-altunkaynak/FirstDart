db4748199201e4a3abd4031e86638337f3f5551e
Abdullah ALTUNKAYNAK <58576152+abdullah-altunkaynak@users.noreply.github.com>
2022-09-12 21:31:48 +0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Changes to be committed:
    	modified:   main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,12 @@
 void main() {
   print('Hello World!');
+  //primitive variables
+  String message = 'Hi String';
+  print(message);
+  int dateOfBirth = 1999;
+  print('Date Of Birth: ' + dateOfBirth.toString());
+  double age = 23.5;
+  print('Age: ' + age.toString());
+  bool isTrue = true;
+  print('isTrue: ' + isTrue.toString());
 }
